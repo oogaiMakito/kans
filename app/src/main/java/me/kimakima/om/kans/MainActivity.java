b92eaf7b2f0ea3c677212326b39dd1fa5d037737
oogaiMakito <makito.man@gmail.com>
2015-11-26 20:09:06 +0900

    start

--- app/src/main/java/me/kimakima/om/kans/MainActivity.java
+++ app/src/main/java/me/kimakima/om/kans/MainActivity.java
@@ -1,17 +1,26 @@
 package me.kimakima.om.kans;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.AlertDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -25,26 +34,51 @@ public class MainActivity extends Activity {
         SQLiteDatabase db = helper.getReadableDatabase();
 
         //queryメソッドの実行例
-        Cursor c = db.query("purchaseHis", new String[]{"kanid", "kanname", "price", "date"}, null, null, null, null, null);
+        Cursor c = db.query("purchaseHis", new String[]{"_id","kanid", "kanname", "price", "date"}, null, null, null, null, "_id DESC");
+        //Cursor cursor = db.query(TABLE_NAME, COLUMNS, null, null, null, null, COLUMN_CREATED_TIME + " DESC")
 
         //先頭に移動
         c.moveToFirst();
 
         //総件数を取得
-        String[] list = new String[c.getCount()];
+        Integer list_length = c.getCount();
 
-        //ループでlist配列に値を代入
-        for (int i = 0; i < list.length; i++) {
-            list[i] = String.format("%s \n %s \n %s / %d円", c.getString(0), c.getString(3), c.getString(1), c.getInt(2));
+        //総件数を取得
+        List<ListItem> list = new ArrayList<ListItem>();
+
+        for (int i = 0; i < list_length; i++) {
+            ListItem item = new ListItem();
+
+            //画像名を取得
+            int imgId = getResources().getIdentifier(c.getString(1),"drawable",getPackageName());
+            //テキストを取得
+            item.setText(String.format("%s\n %s\n 値段: %d円", c.getString(4), c.getString(2), c.getInt(3)));
+
+            item.setImageId(imgId);
+            list.add(item);
+
             c.moveToNext();
         }
         c.close();
         db.close();
 
-        //「simple_list_item_1」は、もともと用意されている定義済みのレイアウトファイルのID
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        // adapterのインスタンスを作成
+        ImageArrayAdapter adapter = new ImageArrayAdapter(this, R.layout.list_view_image_item, list);
         ListView listView = (ListView)findViewById(R.id.listView);
-        listView.setAdapter(arrayAdapter);
+        listView.setAdapter(adapter);
+
+        //リスト項目が長押しされた時のイベントを追加
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String msg = position + "番目のアイテムが長押しされました";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+
+
     }
 
 
